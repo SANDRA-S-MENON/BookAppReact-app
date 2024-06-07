c600520a4a7b99c96b1c96105ee7ea24533206ef
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-06-07 13:01:03 +0530

    jsx created  successfully

--- src/components/DeleteBook.jsx
+++ src/components/DeleteBook.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const DeleteBook = () => {
   return (
     <div>
+        <h1><center>Delete Book</center></h1>
         <div className="container">
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+const ViewAll = () => {
+  return (
+    <div>
+        <br />
+        <div className="container">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sno</th>
+      <th scope="col">Book Title</th>
+      <th scope="col">Author</th>
+      <th scope="col">Publisher</th>
+      <th scope="col">Price</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tom n jerry</td>
+      <td>mark</td>
+      <td>dc</td>
+      <td>250</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Tom n jerry</td>
+      <td>mark</td>
+      <td>dc</td>
+      <td>250</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Tom n jerry</td>
+      <td>mark</td>
+      <td>dc</td>
+      <td>250</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Tom n jerry</td>
+      <td>mark</td>
+      <td>dc</td>
+      <td>250</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Tom n jerry</td>
+      <td>mark</td>
+      <td>dc</td>
+      <td>250</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Tom n jerry</td>
+      <td>mark</td>
+      <td>dc</td>
+      <td>250</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Tom n jerry</td>
+      <td>mark</td>
+      <td>dc</td>
+      <td>250</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Tom n jerry</td>
+      <td>mark</td>
+      <td>dc</td>
+      <td>250</td>
+    </tr>
+  </tbody>
+</table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default ViewAll
